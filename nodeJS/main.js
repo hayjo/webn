ccf70a1c73874fed3675f8da2a942e97ad6f6679
h <hay.hyunjo@gmail.com>
2020-09-16 20:28:35 +0900

    add CRUD functions

--- nodeJS/main.js
+++ nodeJS/main.js
@@ -2,43 +2,108 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var templates = require('./template');
+var qs = require('querystring');
 
 var app = http.createServer(function(request, response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var title = queryData.id;
     var pathname = url.parse(_url, true).pathname;
-    if(pathname !== '/'){
+    if(pathname !== '/' && pathname.indexOf('.') !== -1){
       var ext = pathname.split('.').splice(-1)[0];
     } else {
       var ext = '';
     }
-    console.log(title, pathname);
 
 
     fs.readdir('./data', function(err, dataList){
-      console.log(ext);
       if(ext){
-        if(ext === '.css'){
+        if(ext === 'css'){
           response.writeHead(200, {'Content-Type': 'text/css'});
-        } else if(ext === '.js'){
+          response.write(fs.readFileSync(__dirname + pathname, 'utf-8'));
+          response.end();
+        } else if(ext === 'js'){
           response.writeHead(200, {'Content-Type': 'text/javascript'});
-        }
-        response.write(fs.readFileSync(__dirname + pathname, 'utf-8'));
-        response.end();
-      } else {
-        if(pathname === "/" && title === undefined){
-            console.log();
-            var template = templates.get('home', `./home`, dataList);
-            response.writeHead(200);
-            response.end(template);
-        } else if(dataList.indexOf(title) >= 0){
-            var template = templates.get(title, `data/${title}`, dataList);
-            response.writeHead(200);
-            response.end(template);
+          response.write(fs.readFileSync(__dirname + pathname, 'utf-8'));
+          response.end();
         } else {
           response.writeHead(404);
           response.end('Page Not Founded');
+        }
+      } else {
+        if(pathname === "/" && title === undefined){
+            var template = templates.get('home', `./home`, dataList);
+            response.writeHead(200);
+            response.end(template);
+        } else if(pathname === "/" && dataList.indexOf(title) >= 0){
+            var template = templates.get(title, `data/${title}`, dataList);
+            response.writeHead(200);
+            response.end(template);
+        } else if (pathname === "/create") {
+            var template = templates.get('create', '/create', dataList);
+            response.writeHead(200);
+            response.end(template);
+        } else if (pathname === "/process_create") {
+            var body = '';
+            request.on('data', function(data){ //
+              body += data;
+            });
+            request.on('end', function(){
+              var post = qs.parse(body);
+              var des = JSON.stringify(post);
+              fs.writeFile(`data/${post.title}`, des, 'utf8', function(err){
+                response.writeHead(302, {location: `/?id=${post.title}`});
+                response.end();
+              })
+            });
+        } else if (pathname === "/update") {
+          if(title !== "update"){
+            var template = templates.get('update', `data/${title.toLowerCase()}`, dataList);
+            response.writeHead(200);
+            response.end(template);
+          } else {
+            response.writeHead(302, {location: `/`});
+            response.end(template);
+          }
+        } else if (pathname === "/process_update") {
+            var body = '';
+            request.on('data', function(data){ //
+              body += data;
+            });
+            request.on('end', function(){
+              var post = qs.parse(body);
+              if(post.id === post.title){
+                var des = JSON.stringify(post);
+                fs.writeFile(`data/${post.title}`, des, 'utf8', function(err){
+                  response.writeHead(302, {location: `/?id=${post.title}`});
+                  response.end();
+                });
+              } else {
+                fs.rename(`data/${post.id}`, `data/${post.title}`, function(err){
+                  var des = JSON.stringify(post);
+                  des.id = des.title;
+                  fs.writeFile(`data/${post.title}`, des, 'utf8', function(err){
+                    response.writeHead(302, {location: `/?id=${post.title}`});
+                    response.end();
+                  });
+                });
+              }
+            });
+        } else if (pathname === "/process_delete") {
+          var body = '';
+          request.on('data', function(data){ //
+            body += data;
+          });
+          request.on('end', function(){
+            var post = qs.parse(body);
+            fs.unlink(`data/${post.id}`, function(err){
+              response.writeHead(302, {location: `/`});
+              response.end();
+            });
+          });
+        } else {
+            response.writeHead(404);
+            response.end('Page Not Founded');
         }
       }
     })
--- nodeJS/template.js
+++ nodeJS/template.js
@@ -12,17 +12,27 @@ module.exports = {          // import를 위 객체에 할당
       menu = menu + ` id=active`       // active는 메뉴상에서 red 처리
     };
     menu = menu + `>Home</a></li>
-                <li>Contact</li>
-              </ul>
+                <li><a href="/create" class="edits"`;
+    if(title === 'create'){
+      menu = menu + ` id=active`
+    };
+    menu = menu + `>Create</a></li>`
+    if(['create', 'home', 'update', 'delete'].indexOf(title) < 0){
+      menu = menu + `<li><a href="/update?id=${title}" class="edits">Update</a></li>
+      <li><form action="process_delete" method="post" onsubmit="">
+        <input type="hidden" name="id" value="${title}">
+        <input type="submit" value="Delete" class="edits">
+      </form></li>`;
+    }
+    menu = menu + `</ul>
             </nav>
           </div>
                  <div class="list">
                   <ol>`;
     for (var com of companies){
       menu = menu + `
-      <li><a href="?id=${com}"
+      <li><a href="/?id=${com}"
       `
-      console.log(com, title);
       if(title === com){
         menu = menu + ` id="active"` // active는 메뉴상에서 red처리
       }
@@ -35,7 +45,8 @@ module.exports = {          // import를 위 객체에 할당
                 </div>`
     return menu;
   },
-  getContent: function(capTitle, data){
+  getContent: function(title, data){
+    var capTitle = this.capitalize(title);
     function getInfo(infoList){
       var result = '<ul>';
       for (var key in infoList){
@@ -53,7 +64,7 @@ module.exports = {          // import를 위 객체에 할당
     }
     var description = data.description;
 
-    if(capTitle === 'Home'){
+    if(title === 'home'){
       return `      <div class="right">
           <div class="comlogo">
             <img src="${logo} ${logosize}">
@@ -63,11 +74,18 @@ module.exports = {          // import를 위 객체에 할당
           </p>
         </div>`
     } else {
-      var infoList = data.info;
-      var info = getInfo(infoList);
+      if (typeof(data.info) === typeof({})){
+        var infoList = data.info;
+        var info = getInfo(infoList);
+      } else if (typeof(JSON.parse(data.info)) === typeof({})){
+        var infoList = JSON.parse(data.info);
+        var info = getInfo(infoList);
+      } else {
+        var info = data.info;
+      }
       return `      <div class="right">
           <div class="comlogo">
-            <img src="${logo}">
+            <img class="logo" src="${logo}">
           </div>
           <h2>${capTitle}</h2>
           <h4>Company Info</h4>
@@ -90,13 +108,13 @@ module.exports = {          // import를 위 객체에 할당
     <div class="buttons">
     `
     if(i > 0){
-      var b1 = `<div class="button1"><button onclick="location.href='?id=${dataList[i-1]}'">Previous</button></div>
+      var b1 = `<div class="button1"><button onclick="location.href='/?id=${dataList[i-1]}'">Previous</button></div>
       `;
     } else {
       var b1 = '<div class="button1"></div>';
     };
-    if(i < dataList.length-1){
-      var b2 = `<div class="button2"><button onclick="location.href='?id=${dataList[i+1]}'">Next</button></div>
+    if(0 <= i && i < dataList.length-1){
+      var b2 = `<div class="button2"><button onclick="location.href='/?id=${dataList[i+1]}'">Next</button></div>
       `;
     } else {
       var b2 = '<div class="button2"></div>'
@@ -128,17 +146,62 @@ module.exports = {          // import를 위 객체에 할당
     </html>
     `
   },
-  get: function(title, filePath, dataList){
-    var dataBuffer = fs.readFileSync(filePath);
-    var dataJSON = dataBuffer.toString();
-    var data = JSON.parse(dataJSON)
-    var capTitle = this.capitalize(title);
+  getForm: function(data){
+    var data_title = '';
+    var data_logo = '';
+    var data_info = '';
+    var data_desc = '';
+    var hidden = '';
+    var action = '/process_create';
 
+    if (data !== ''){
+      data_title = ` value="${data.title}"`;
+      data_logo = `${data.logo}`;
+      data_info = `${JSON.stringify(data.info)}`;
+      data_desc = `${data.description}`;
+      hidden = `<input type="hidden" name="id" value="${data.title}">`
+      action = `/process_update`;
+    }
+    return `
+    <form action="` + action +`" method="post" id="postform">
+      <p>` + hidden + `
+        <h5>Title</h5>
+        <input type="text" name="title" placeholder="title"` + data_title + `></p>
+      <p>
+        <h5>Logo URL</h6>
+        <textarea name="logo" placeholder="logo" rows="1" cols="80">` + data_logo + `</textarea>
+      </p>
+      <p>
+        <h5>Info</h6>
+        <textarea name="info" placeholder="info" rows="3" cols="80">` + data_info + `</textarea>
+      </p>
+      <p>
+        <h5>Description</h6>
+        <textarea name="description" rows="5" cols="80" placeholder="description">` + data_desc + `</textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
+    `;
+  },
+  get: function(title, filePath, dataList){
+    var capTitle = this.capitalize(title);
+    var button = this.getButton(title, dataList);
+    if (title === 'create'){ // create 페이지
+      var content = this.getForm('');
+    } else { // 본문 있는 페이지
+      var dataBuffer = fs.readFileSync(filePath);
+      var dataJSON = dataBuffer.toString();
+      var data = JSON.parse(dataJSON);
+      var content = this.getContent(title, data);
+      if (title === 'update'){
+        content = this.getForm(data);
+      };
+    }
     var pageTitle = `<title>Big ${dataList.length} Tech - ${capTitle}</title>`;
     var pageHeading = `<h1>Big ${dataList.length} Tech Companies</h1>`;
     var menu = this.getMenu(title, dataList);
-    var button = this.getButton(title, dataList);
-    var content = this.getContent(capTitle, data);
 
     var body = this.getBody(pageTitle, pageHeading, menu, button, content);
     return body;
